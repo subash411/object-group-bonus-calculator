31ec5fb51f55bf1f97e576f9fd857c109d48d150
Subash Khatri <subashkhatri44@gmail.com>
2021-12-07 16:29:24 -0600

    complete

--- client.js
+++ client.js
@@ -30,7 +30,6 @@ const employees = [
     reviewRating: 1
   }
 ];
-
 // YOU SHOULD NOT NEED TO CHANGE ANYTHING ABOVE THIS POINT
 
 // This problem is massive! Break the problem down, take small steps, and test as you go.
@@ -39,4 +38,40 @@ const employees = [
 // This is not a race. Everyone on your team should understand what is happening.
 // Ask questions when you don't.
 
+
 console.log( employees );
+for (let zeep of employees) {
+  console.log(zeep);
+};
+function EmployeeMaker(employee) {
+  this.name = employee.name;
+  bonusPercentage = 0;
+  //console.log('what is the review rating', employee.reviewRating);
+  if (employee.reviewRating === 2) {
+    bonusPercentage = 0;
+  }
+  else if (employee.reviewRating === 3) {
+    bonusPercentage = 0.04;
+    //console.log('in line 53', bonusPercentage, employee.reviewRating);
+  }
+  else if (employee.reviewRating === 4) {
+    bonusPercentage = 0.06;
+  }
+  else if (employee.reviewRating === 5) {
+    bonusPercentage = 0.1;
+  }
+  else if (employee.employeeNumber >= 1000) {
+    bonusPercentage = bonusPercentage + 0.05;
+  }
+  else if (employee.annualSalary > 65000){
+    bonusPercentage = bonusPercentage - 0.01;
+  }
+  if (bonusPercentage > 0.13, bonusPercentage < 0){
+    bonusPercentage = undefined;
+  }
+  this.bonusPercentage = bonusPercentage,
+  this.totalCompensation = employee.annualSalary + bonusPercentage,
+  this.totalBonus = Math.round(this.totalCompensation);
+};
+console.log(new EmployeeMaker(employees[0]));
+console.log(new EmployeeMaker(employees[1]));
